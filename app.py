41c2950b39a0e1a995ef423c721f8abd12c8234d
Hiren Joshi <hirenj@gmail.com>
2017-10-10 16:56:34 +0200

    Add in proper auth dance to get data

--- app.py
+++ app.py
@@ -1,13 +1,40 @@
 import rumps
 import keyring
 import os
+import requests
+
 path = os.path.dirname(os.path.realpath(__file__))
 path_to_img = '%s/pony.jpg' % path
+
+from requests_oauthlib import OAuth2Session
+from oauthlib.oauth2 import BackendApplicationClient
 
 endpoint = 'https://test.glycocode.com'
 
 client_id = keyring.get_password('gatordata.client_id', endpoint)
 client_secret = keyring.get_password('gatordata.client_secret', endpoint)
+
+print '%s/api/config' % endpoint
+
+config_info = requests.get('%s/api/login/config' % endpoint )
+
+auth0_domain = config_info.json()['AUTH0_DOMAIN']
+audience = config_info.json()['API_AUDIENCE']
+
+client = BackendApplicationClient(client_id=client_id,audience=audience)
+
+gator = OAuth2Session(client=client)
+
+token_uri = ('https://%s.auth0.com/oauth/token' % auth0_domain)
+
+token = gator.fetch_token(token_url=token_uri,client_id=client_id,client_secret=client_secret,audience=audience)
+
+gator.headers.update({'x-api-key' : client_id })
+
+url = 'https://test.glycocode.com/api/metadata'
+r = gator.get(url)
+
+user_metadata = r.json()
 
 rumps.debug_mode(True)
 
